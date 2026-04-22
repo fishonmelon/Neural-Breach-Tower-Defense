a2c4516b462eea4adeb8758568f8a387fccbd2a8
fishonmelon <spaytonjr.092012@icloud.com>
2026-04-22 18:27:35 -0400

    Create frontend/vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true,
+      },
+    },
+  },
+});
