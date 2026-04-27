60612ded6cc79546a473e18433c7d9fca74dfb2a
fishonmelon <spaytonjr.092012@icloud.com>
2026-04-27 19:13:53 -0400

    Create App.svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Game from './components/Game.svelte';
+</script>
+
+<main>
+  <Game />
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    background: #0a0a0f;
+  }
+</style>
